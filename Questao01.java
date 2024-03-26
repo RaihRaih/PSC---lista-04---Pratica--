f8628dffb2a75e6b0df8e634e76dedf8463fb818
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-03-25 21:03:56 -0300

    Create Questao01.java

---
+++ Questao01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Questao01
+{
+	public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.im);
+        int produtoImpar, somaPar, numero;
+        
+        while (numero != -1){
+            
+        }
+        
+        
+	}
+}
